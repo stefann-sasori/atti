f8a1ee2f06fdc6e00927ed24c0a99f30ab349e4f
stefann <stefann.sasori@gmail.com>
2022-12-12 21:05:26 +0000

    locked

--- src/views/Locked.vue
+++ src/views/Locked.vue
@@ -14,7 +14,7 @@
       </div>
 
     </div>
-    <div>
+    <div style="margin-top: 10rem">
       <p style="color: orange">Toutes les opérations sur ce compte sont désactivées, merci de rentrer en contact immédiatement avec votre gestionnaire de compte de manière directe ou par mail à l'adresse:
         <a href="mailto:contact@uba-afrique.me">contact@uba-afrique.me</a>.
       </p>
